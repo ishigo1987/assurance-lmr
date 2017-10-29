0ebe9c5d15f450c3fd80c361d42306c1c72c0355
ishigo1987 <stephnguets@yahoo.fr>
2017-10-29 14:05:50 +0100

    Changements importants 3 premieres categories faits

--- src/modules/calculDevis.js
+++ src/modules/calculDevis.js
@@ -5,12 +5,13 @@ module.exports = (devisData)=>{
       const zone = devisData.zone;
       const dureeJour = devisData.dureeJour;
       const dureeMois = devisData.dureeMois;
+      const energy = devisData.energy;
+      let puissance = devisData.puissance;
+      if(puissance === 0){puissance = 1;}
+      const remorque = devisData.remorque;
       let rca;
       if(category === "Catégorie 1"){
-         const energy = devisData.energy;
-         const puissance = devisData.puissance;
-         const remorque = devisData.remorque;
-        // Test de le Zone
+        // Test de Zone
          if(zone === "Zone A"){
            // On teste la valeur de la puissance et on verifie si il y'a une remorque ou pas
            if(puissance <= 2 && remorque === "Non" && (energy === "Essence" || energy === "Diesel")){rca = 52499;}
@@ -40,28 +41,107 @@ module.exports = (devisData)=>{
            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Non" ){rca = 133077;}
            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Oui" ){rca = 148388;}
          }else if(zone === "Zone C"){
-
+            // On teste la valeur de la puissance et on verifie si il y'a une remorque ou pas
+            if(puissance <= 2 && remorque === "Non" && (energy === "Essence" || energy === "Diesel")){rca = 48124;}
+            if(puissance <= 2 && remorque === "Oui" && (energy === "Essence" || energy === "Diesel")){rca = 62935;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Non" ){rca = 58468;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Oui" ){rca = 64315;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 7 )) && remorque === "Non" ){rca = 74970;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 7 )) && remorque === "Oui" ){rca = 71467;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Non" ){rca = 84789;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Oui" ){rca = 93268;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Non" ){rca = 107951;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Oui" ){rca = 118749;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Non" ){rca = 127291;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Oui" ){rca = 140220;}      
          }
       }else if(category === "Catégorie 2"){
-          // Test de le Zone
+          // Test de Zone
         if(zone === "Zone A"){
-            
+           // On teste la valeur de la puissance et on verifie si il y'a une remorque ou pas
+           if(puissance <= 2 && remorque === "Non" && (energy === "Essence" || energy === "Diesel")){rca = 58759;}
+           if(puissance <= 2 && remorque === "Oui" && (energy === "Essence" || energy === "Diesel")){rca = 70504;}
+           if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Non" ){rca = 71183;}
+           if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Oui" ){rca = 85420;}
+           if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 7 )) && remorque === "Non" ){rca = 80918;}
+           if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 7 )) && remorque === "Oui" ){rca = 97102;}
+           if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 6 && puissance < 11 )) && remorque === "Non" ){rca = 121212;}
+           if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 6 && puissance < 11 )) && remorque === "Oui" ){rca = 145454;}
+           if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Non" ){rca = 150086;}
+           if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Oui" ){rca = 180103;}
+           if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 16)) && remorque === "Non" ){rca = 173591;}
+           if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 16)) && remorque === "Oui" ){rca = 208309;} 
         }else if(zone === "Zone B"){
-            
+            if(puissance <= 2 && remorque === "Non" && (energy === "Essence" || energy === "Diesel")){rca = 56311;}
+            if(puissance <= 2 && remorque === "Oui" && (energy === "Essence" || energy === "Diesel")){rca = 67566;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Non" ){rca = 68217;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Oui" ){rca = 81860;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Non" ){rca = 77547;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Oui" ){rca = 93056;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Non" ){rca = 116162;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Oui" ){rca = 139394;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Non" ){rca = 143833;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Oui" ){rca = 172599;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Non" ){rca = 166358;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Oui" ){rca = 199630;}
         }else if(zone === "Zone C"){
-            
-        }
+            if(puissance <= 2 && remorque === "Non" && (energy === "Essence" || energy === "Diesel")){rca = 53863;}
+            if(puissance <= 2 && remorque === "Oui" && (energy === "Essence" || energy === "Diesel")){rca = 64628;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Non" ){rca = 65251;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Oui" ){rca = 78301;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Non" ){rca = 74175;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Oui" ){rca = 89010;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Non" ){rca = 111111;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Oui" ){rca = 133333;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Non" ){rca = 133333;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Oui" ){rca = 165095;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Non" ){rca = 159125;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Oui" ){rca = 190950;}  
+        } 
       }else if(category === "Catégorie 3"){
-        // Test de le Zone
+        // Test de Zone
         if(zone === "Zone A"){
-            
+            if(puissance <= 2 && remorque === "Non" && (energy === "Essence" || energy === "Diesel")){rca = 94044;}
+            if(puissance <= 2 && remorque === "Oui" && (energy === "Essence" || energy === "Diesel")){rca = 122257;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Non" ){rca = 116045;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Oui" ){rca = 150858;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Non" ){rca = 132900;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Oui" ){rca = 172771;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Non" ){rca = 196282;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Oui" ){rca = 255167;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Non" ){rca = 250210;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Oui" ){rca = 325272;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Non" ){rca = 290326;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Oui" ){rca = 377426;}  
         }else if(zone === "Zone B"){
-            
+            if(puissance <= 2 && remorque === "Non" && (energy === "Essence" || energy === "Diesel")){rca = 90126;}
+            if(puissance <= 2 && remorque === "Oui" && (energy === "Essence" || energy === "Diesel")){rca = 117163;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Non" ){rca = 111210;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Oui" ){rca = 144572;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Non" ){rca = 127363;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Oui" ){rca = 165572;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Non" ){rca = 188103;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Oui" ){rca = 244535;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Non" ){rca = 239784;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Oui" ){rca = 311719;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Non" ){rca = 278229;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Oui" ){rca = 361700;}  
         }else if(zone === "Zone C"){
-            
+            if(puissance <= 2 && remorque === "Non" && (energy === "Essence" || energy === "Diesel")){rca = 86207;}
+            if(puissance <= 2 && remorque === "Oui" && (energy === "Essence" || energy === "Diesel")){rca = 112069;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Non" ){rca = 106374;}
+            if(((energy === "Essence" && puissance > 2 && puissance < 7)||(energy === "Diesel" && puissance > 1 && puissance < 5 )) && remorque === "Oui" ){rca = 138287;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Non" ){rca = 121825;}
+            if(((energy === "Essence" && puissance > 6 && puissance < 11)||(energy === "Diesel" && puissance > 4 && puissance < 6 )) && remorque === "Oui" ){rca = 158374;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Non" ){rca = 179925;}
+            if(((energy === "Essence" && puissance > 10 && puissance < 15)||(energy === "Diesel" && puissance > 7 && puissance < 11 )) && remorque === "Oui" ){rca = 233903;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Non" ){rca = 229359;}
+            if(((energy === "Essence" && puissance > 14 && puissance < 24)||(energy === "Diesel" && puissance > 10 && puissance < 17 )) && remorque === "Oui" ){rca = 298166;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Non" ){rca = 266132;}
+            if(((energy === "Essence" && puissance > 23)||(energy === "Diesel" && puissance > 17)) && remorque === "Oui" ){rca = 345974;} 
         }
       }else if(category === "Catégorie 4"){
-        // Test de le Zone
+        // Test de Zone
         if(zone === "Zone A"){
             
         }else if(zone === "Zone B"){
@@ -70,7 +150,7 @@ module.exports = (devisData)=>{
             
         }
       }else if(category === "Catégorie 5"){
-         // Test de le Zone
+         // Test de Zone
         if(zone === "Zone A"){
             
         }else if(zone === "Zone B"){
