4a76ba9b6146784cecea13aa310de6bccba3781c
ishigo1987 <stephnguets@yahoo.fr>
2017-12-28 14:43:41 +0100

    Modifications Mineures

--- src/modules/notifications.js
+++ src/modules/notifications.js
@@ -7,18 +7,20 @@ module.exports = ()=>{
     const returnResponse = ajaxGetNewResponse(dataToSend,'https://www.afrikhealth.com/apiAssuranceLmr/apiHome.php');
           returnResponse.then((response)=>{
            // Notification pour avertir l'utilisateur d'une réponse a sa question
-           if(localStorage.getItem('activePage') === null){
-            cordova.plugins.notification.local.hasPermission(function (granted) {
-                cordova.plugins.notification.local.schedule({
-                    title: 'Réponse de Msr Assurance',
-                    text: 'Vous avez une réponse a votre question',
-                    foreground: true,
-                    badge:true,
-                    smallIcon:'res://android/ldpi.png'
+           if(response.Statut === "Réponse envoyée"){
+            if(localStorage.getItem('activePage') === null){
+                cordova.plugins.notification.local.hasPermission(function (granted) {
+                    cordova.plugins.notification.local.schedule({
+                        title: 'Réponse de Msr Assurance',
+                        text: response.ReponseLmr,
+                        foreground: true,
+                        badge:true,
+                        smallIcon:'res://android/ldpi.png'
+                    });
+                    function openSpeakToAnAgentPage(){require('./speakToAnAgent.js')(executeNavigationView).appendTo(executeNavigationView);}
+                    cordova.plugins.notification.local.on('click', openSpeakToAnAgentPage);
                 });
-                function openSpeakToAnAgentPage(){require('./speakToAnAgent.js')(executeNavigationView).appendTo(executeNavigationView);}
-                cordova.plugins.notification.local.on('click', openSpeakToAnAgentPage, scope);
-            });
+               }
            }
          }).catch((error)=>{
            console.log(error);
